ea29207934de7dccca635157aa33325a0c573534
楊斯惟 <yangszwei@outlook.com>
2024-01-11 09:53:30 +0800

    Change mongo version to 4.4

--- scripts/activate-user.sh
+++ scripts/activate-user.sh
@@ -3,4 +3,4 @@
 MONGO=$(docker compose ps -aq "mongo")
 COMMAND="db.users.updateOne({account: \"$1\"}, {\$set: {status: 1}});"
 
-docker exec "$MONGO" mongosh localhost/Micala -u root -p root --authenticationDatabase admin --eval "$COMMAND"
+docker exec "$MONGO" mongo localhost/Micala -u root -p root --authenticationDatabase admin --eval "$COMMAND"
